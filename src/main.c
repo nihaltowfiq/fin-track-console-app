fdcbe8560d01aa9c4b6ff136db30c2a548308bf2
nihaltowfiq <nihaltowfiq@gmail.com>
2025-08-07 00:33:38 +0600

    feat: formatter added

--- src/main.c
+++ src/main.c
@@ -2,6 +2,32 @@
 #include <stdlib.h>
 
 int main() {
-  printf("Hello world!\n");
+  int choice;
+
+  printf("==== Welcome to FinTrack ====\n");
+
+  while (1) {
+    printf("\n1. Login\n2. Sign Up\n3. Exit\n");
+    printf("Choose option: ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+      case 1:
+        // call login()
+        break;
+      case 2:
+        // call signup()
+        break;
+      case 3:
+        printf("Exiting FinTrack...\n");
+        exit(0);
+        break;
+
+      default:
+        // call
+        printf("Invalid option!\n");
+    }
+  }
+
   return 0;
 }
