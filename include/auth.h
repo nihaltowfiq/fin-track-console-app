503020f077102f2a26e022115cdde3f1b0faa481
nihaltowfiq <nihaltowfiq@gmail.com>
2025-08-27 23:46:28 +0600

    removed all files

--- include/auth.h
+++
@@ -1,9 +0,0 @@
-#ifndef AUTH_H
-#define AUTH_H
-
-#include <stdbool.h>
-
-bool signup(const char *username, const char *password);
-bool login(const char *username, const char *password);
-
-#endif
--- include/config.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-#define USERS_DB_PATH "data/users.txt"
-#define USERNAME_MAX 32
-#define PASSWORD_MAX 64
-
-#endif
